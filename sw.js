d91b9f043e1c18b91f91ebd1978abcf0467ea801
murilosreis <murilofreak@gmail.com>
2026-07-28 00:47:54 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'minha-rota-v14';
+const CACHE_NAME = 'minha-rota-v15';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
